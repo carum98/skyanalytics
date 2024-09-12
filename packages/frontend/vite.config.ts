c0fe6e70c7c2c62e8b45d48569a59e3bf505aab2
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-09-12 00:58:31 +0000

    frontend: update dependencies and rename baseUrl to /app

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
-      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
-      '@ui': fileURLToPath(new URL('./src/components/ui', import.meta.url)),
+      '@': fileURLToPath(new URL('./app', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./app/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./app/components', import.meta.url)),
+      '@views': fileURLToPath(new URL('./app/views', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./app/composables', import.meta.url)),
+      '@ui': fileURLToPath(new URL('./app/components/ui', import.meta.url)),
     }
   }
 })
